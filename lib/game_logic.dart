42fe519a73b54a79b320f5d90145e28b03fe20e7
AshishTaduce <ashishteddy2@gmail.com>
2020-02-27 09:16:05 +0530

    Basic UI implemented

---
+++ lib/game_logic.dart
@@ -0,0 +1,122 @@
+import 'dart:math';
+
+void main(){
+  List<List<int>> board = [
+    [0,0,0,0],
+    [0,0,2,0],
+    [0,18,0,0],
+    [0,0,0,0],
+  ];
+  Start2048 game = Start2048(board);
+  game.printBoard();
+  game.upSwipe();
+
+  game.randomZeroPosition();
+  game.printBoard();
+}
+
+class Start2048{
+  List<List<int>> board;
+  Start2048(this.board);
+
+  void randomZeroPosition(){
+    final _random = new Random();
+    List<List<int>> possiblePositions = [];
+    for(int i = 0; i < board.length; i++){
+      for(int j = 0; j < board.length; j++){
+        if (board[i][j] == 0) possiblePositions.insert(0, [i,j],);
+      }
+    }
+    int randomPosition = _random.nextInt(possiblePositions.length);
+    print(possiblePositions[randomPosition]);
+    board[possiblePositions[randomPosition][0]][possiblePositions[randomPosition][1]] = 2;
+  }
+
+  void rightSwipe(){
+    for(int i = 0; i < board.length; i++){
+      rightSlide(board[i]);
+    }
+  }
+
+  void  leftSwipe(){
+    for(int i = 0; i < board.length; i++){
+      leftSlide(board[i]);
+    }
+  }
+
+  List<List<int>>  transposeArray(array, arrayLength){
+    List<List<int>> newArray = [];
+    for(var i = 0; i < array.length; i++){
+      newArray.insert(0,[]);
+    }
+    for(var i = 0; i < array.length; i++){
+      for(var j = 0; j < arrayLength; j++){
+        newArray[j].insert(0,array[i][j]);
+      }
+    }
+    return newArray;
+  }
+
+  void  upSwipe(){
+    board = transposeArray(board,board.length);
+    leftSwipe();
+    board = transposeArray(board,board.length);
+  }
+
+  void  downSwipe(){
+    board = transposeArray(board,board.length);
+    rightSwipe();
+    board = transposeArray(board,board.length);
+  }
+
+  List<int>  leftSlide(List<int>  row){
+    int listLength = row.length;
+    while(row.contains(0)){
+      for(int j = 0; j < row.length; j++){
+        if(row[j] == 0) row.removeAt(j);
+      }}
+    for(int i = 0; i < row.length - 1; i++){
+      if(row[i] == row[i+1]){
+        row[i] = row[i] + row[i];
+        row.removeAt(i+1);
+        row.insert(row.length - 1, 0);
+      }
+    }
+    while(row.length != listLength){
+      row.insert(row.length, 0);
+    }
+    return row.toList();
+  }
+
+  List<int> rightSlide(List<int>row){
+    int listLength = row.length;
+    while(row.contains(0)){
+      for(int j = 0; j < row.length; j++){
+        if(row[j] == 0) row.removeAt(j);
+      }}
+    for(int i = 0; i < row.length - 1; i++){
+      if(row[i] == row[i+1]){
+        row[i] = row[i] + row[i];
+        row.removeAt(i+1);
+        row.insert(row.length - 1, 0);
+      }
+    }
+    while(row.length != listLength){
+      row.insert(0, 0);
+    }
+    return row.toList();
+  }
+
+  bool checkItGameEnded(array){
+    return (array.find((int element) => element == 2048) != -1 && array.find((int element) => element == 0) != 1);
+  }
+
+  bool checkIfValidMovesLeft(List<List<int>> board, List<int> cellPosition){
+  }
+
+  void printBoard(){
+    print(board);
+  }
+
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:game_2048/game_logic.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: GameBoard(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class GameBoard extends StatefulWidget {
+  GameBoard({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _GameBoardState createState() => _GameBoardState();
+}
+
+class _GameBoardState extends State<GameBoard> {
+  List<List<int>> board = [
+    [0,0,0,0],
+    [0,0,2,0],
+    [0,18,0,0],
+    [0,0,0,0],
+  ];
+
+Start2048 gameLogic;
+  @override
+  void initState() {
+  gameLogic = Start2048(board);
+  super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> listOfWidgets = [];
+    for(int i = 0; i < 4; i++){
+      listOfWidgets.add(gameRow(gameLogic: gameLogic, rowNumber: i,));
+    }
+    listOfWidgets.add(Row(
+      children: <Widget>[
+        IconButton(icon: Icon(Icons.arrow_left), onPressed:(){
+          gameLogic.leftSwipe();
+          gameLogic.randomZeroPosition();
+          setState(() {});},),
+        IconButton(icon: Icon(Icons.arrow_drop_up), onPressed:(){
+          gameLogic.upSwipe();
+          gameLogic.randomZeroPosition();
+          setState(() {});},),
+        IconButton(icon: Icon(Icons.arrow_drop_down), onPressed:(){
+          gameLogic.downSwipe();
+          setState(() {});},),
+        IconButton(icon: Icon(Icons.arrow_right), onPressed:(){
+          gameLogic.rightSwipe();
+          gameLogic.randomZeroPosition();
+          setState(() {});},),
+        IconButton(icon: Icon(Icons.access_alarm), onPressed:(){
+          gameLogic.leftSwipe();
+          gameLogic.randomZeroPosition();
+          setState(() {});},),
+      ],
+    ));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: listOfWidgets,
+        ),
+      ),
+    );
+  }
+}
+
+class gameRow extends StatelessWidget {
+  int rowNumber;
+   gameRow({
+    Key key,
+    @required this.gameLogic,
+    @required this.rowNumber,
+  }) : super(key: key);
+
+  final Start2048 gameLogic;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 75,
+          height: 75,
+          padding: const EdgeInsets.all(8),
+          child: Text(gameLogic.board[this.rowNumber][0].toString()),
+          color: Colors.teal[100],
+        ),
+        Container(
+          width: 75,
+          height: 75,
+          padding: const EdgeInsets.all(8),
+          child: Text(gameLogic.board[this.rowNumber][1].toString()),
+          color: Colors.teal[100],
+        ),
+        Container(
+          width: 75,
+          height: 75,
+          padding: const EdgeInsets.all(8),
+          child: Text(gameLogic.board[this.rowNumber][2].toString()),
+          color: Colors.teal[100],
+        ),
+        Container(
+          width: 75,
+          height: 75,
+          padding: const EdgeInsets.all(8),
+          child: Center(child: Text(gameLogic.board[this.rowNumber][3].toString())),
+          color: Colors.teal[100],
+        ),
+      ],
+    );
+  }
+}
